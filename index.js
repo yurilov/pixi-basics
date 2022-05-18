b9b5738eb47474f607fcfc7ce3d137eff9ada539
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-05-18 21:07:36 +0300

    base functionality

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ const app = new Application({
   view: canvas,
   width: 600,
   height: 500,
-  backgroundColor: 0xff000,
+  backgroundColor: 0x3bf4b,
 });
 
 const gameScene = new Container();
 
 let state = "mainMenu";
-// const mainScene = new Container();
-
+let score = 0;
 function createGameScene(gameScene) {
   const background = new Container();
   gameScene.addChild(background);
@@ -26,13 +25,19 @@ function createGameScene(gameScene) {
   const enemies = new Container();
   gameScene.addChild(enemies);
 
+  const stats = new Container();
+  createScore(gameScene, stats);
+
+  const lives = new Container();
+  gameScene.addChild(lives);
   const sprite = Sprite.from("resources/player.png");
-  sprite.position.x = 100;
-  sprite.position.y = 150;
+  sprite.position.x = app.screen.width * 0.5;
+  sprite.position.y = app.screen.height * 0.9;
   players.addChild(sprite);
 
   let isMouseFlag = false;
   let lastBulletSpawnTime = 0;
+  let livesCount = 3;
   const spawnSpeed = 250;
   const keysMaps = {};
   const speed = 10;
@@ -40,9 +45,19 @@ function createGameScene(gameScene) {
 
   const enemyCount = 10;
 
+  for (let index = 0; index < livesCount; index++) {
+    const heart = Sprite.from("resources/heart.png");
+    heart.width = 20;
+    heart.height = 20;
+    heart.position.x = app.screen.width - 25 - index * 25;
+    heart.position.y = 5;
+
+    lives.addChild(heart);
+  }
+
   for (let index = 0; index < enemyCount; index++) {
     const enemy = Sprite.from("resources/enemy.png");
-    enemy.position.x = index * 50;
+    enemy.position.x = index * 60;
     enemy.position.y = 50;
     enemies.addChild(enemy);
   }
@@ -104,6 +119,8 @@ function createGameScene(gameScene) {
       for (const enemy of enemies.children) {
         if (enemy.getBounds().intersects(bullet.getBounds())) {
           enemies.removeChild(enemy);
+          score += 1;
+          updateScore(stats);
         }
         if (enemies.children.length === 0) {
           state = "winScreen";
@@ -115,7 +132,7 @@ function createGameScene(gameScene) {
     for (const enemy of enemies.children) {
       enemy.position.y += 2 * delay;
       if (enemy.position.y >= app.screen.width) {
-        enemy.position.y = 0;
+        enemy.position.y = 0 + delay;
       }
       for (let index = 0; index < players.children.length; index++) {
         const player = players.children[index];
@@ -123,18 +140,20 @@ function createGameScene(gameScene) {
         if (enemy.getBounds().intersects(player.getBounds())) {
           //   players.removeChild(player);
           // createMainScene(mainScene);
-          state = "mainMenu";
+          enemies.removeChild(enemy);
+          livesCount -= 1;
+          lives.children.pop();
+          // state = "mainMenu";
         }
       }
+    }
+    if (livesCount === 0) {
+      state = "gameOver";
     }
   };
 }
 
 const updateScene = createGameScene(gameScene);
-
-// createMainScene(mainScene);
-
-console.log("Hello, World");
 
 // function createMainScene(mainScene) {
 const mainScene = new Container();
@@ -144,8 +163,8 @@ const field = new Text("Start Game", style);
 field.interactive = true;
 field.buttonMode = true;
 field.scale.x = 2;
-field.position.x = 300;
-field.position.y = 300;
+field.position.x = app.screen.width / 2 - 100;
+field.position.y = app.screen.height / 2;
 mainScene.addChild(field);
 field.on("click", () => {
   state = "game";
@@ -154,10 +173,16 @@ field.on("click", () => {
 });
 
 app.stage.addChild(mainScene);
+
 const winField = new Text("You won", style);
 winField.scale.x = 2;
-winField.position.x = 400;
-winField.position.y = 400;
+winField.position.x = app.screen.width / 2 - winField.width / 2;
+winField.position.y = app.screen.height / 2;
+
+const lostField = new Text("You lost", style);
+lostField.scale.x = 2;
+lostField.position.x = app.screen.width / 2 - winField.width / 2;
+lostField.position.y = app.screen.height / 2;
 app.ticker.add((delay) => {
   if (state === "game") {
     updateScene(delay);
@@ -170,10 +195,27 @@ app.ticker.add((delay) => {
   }
 
   if (state === "winScreen") {
-    app.stage.addChild(mainScene);
-
-    mainScene.addChild(winField);
+    app.stage.addChild(winField);
     app.stage.removeChild(gameScene);
+  }
+
+  if (state === "gameOver") {
+    app.stage.removeChild(gameScene);
+    app.stage.addChild(lostField);
   }
 });
 // }
+
+function createScore(gameScene, stats) {
+  gameScene.addChild(stats);
+  const scoreStyle = new TextStyle({ fill: "#FFFFFF", fontSize: 14 });
+
+  const scoreText = new Text(`Enemies killed: ${score}`, scoreStyle);
+  stats.addChild(scoreText);
+}
+
+function updateScore(stats) {
+  stats.children[0].text = `Enemies killed: ${score}`;
+}
+
+function updateLivesCount() {}
