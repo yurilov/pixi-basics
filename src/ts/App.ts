6c8145a507c622c1d9307aa5b9b67e096ea07ef0
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-05-25 22:34:13 +0300

    basic refactoring done

--- src/ts/App.ts
+++ src/ts/App.ts
@@ -30,7 +30,7 @@ export default class App extends Application {
       3
     );
     this.mainScene = new MainScene(this.gameWidth, this.gameHeight);
-    this.updateScene = this.gameScene.setup();
+    this.updateScene = this.createGameScene();
   }
 
   setup() {
@@ -62,6 +62,129 @@ export default class App extends Application {
     });
   }
 
+  createGameScene() {
+    document.onkeydown = (event) => {
+      this.gameScene.keysMaps[event.code] = true;
+    };
+
+    document.onkeyup = (event) => {
+      this.gameScene.keysMaps[event.code] = false;
+    };
+
+    document.onmousedown = () => {
+      this.gameScene.isMouseFlag = true;
+    };
+
+    document.onmouseup = () => {
+      this.gameScene.isMouseFlag = false;
+    };
+
+    this.gameScene.addChild(this.gameScene.background);
+    this.gameScene.addChild(this.gameScene.spaceShip);
+    this.gameScene.addChild(this.gameScene.bullets);
+    this.gameScene.addChild(this.gameScene.enemies);
+    this.gameScene.addChild(this.gameScene.stats);
+    this.gameScene.addChild(this.gameScene.lives);
+    return (delay: number) => {
+      if (this.gameScene.keysMaps["ArrowLeft"]) {
+        this.gameScene.spaceShip.moveSpriteLeft(delay);
+      }
+      if (this.gameScene.keysMaps["ArrowRight"]) {
+        this.gameScene.spaceShip.moveSpriteRight(delay);
+      }
+      if (this.gameScene.keysMaps["ArrowUp"]) {
+        this.gameScene.spaceShip.moveSpriteUp(delay);
+      }
+      if (this.gameScene.keysMaps["ArrowDown"]) {
+        this.gameScene.spaceShip.moveSpriteDown(delay);
+      }
+
+      if (this.gameScene.isMouseFlag) {
+        const currentTime = Date.now();
+        if (
+          currentTime - this.gameScene.lastBulletSpawnTime >
+          this.gameScene.spawnSpeed
+        ) {
+          this.gameScene.bullets.spawnBullet(this.gameScene.spaceShip);
+          this.gameScene.lastBulletSpawnTime = currentTime;
+        }
+      }
+
+      if (this.gameScene.isDamaged) {
+        const currentTime = Date.now();
+        const checkIfTimePassed =
+          currentTime - this.gameScene.lastCollisionTime >
+          this.gameScene.collisionEffectDuration;
+
+        if (checkIfTimePassed) {
+          this.gameScene.spaceShip.alpha = 1;
+          this.gameScene.isDamaged = true;
+          this.gameScene.lastCollisionTime = 0;
+        }
+      }
+
+      for (
+        let index = 0;
+        index < this.gameScene.bullets.children.length;
+        index += 1
+      ) {
+        const bullet = this.gameScene.bullets.children[index];
+        bullet.position.y -= this.gameScene.bulletSpeed * delay;
+
+        if (bullet.position.y < 0) {
+          this.gameScene.bullets.removeChild(bullet);
+          continue;
+        }
+
+        for (const enemy of this.gameScene.enemies.children) {
+          if (enemy.getBounds().intersects(bullet.getBounds())) {
+            this.gameScene.enemies.removeChild(enemy);
+            this.gameScene.score += 1;
+            this.gameScene.stats.updateScore(this.gameScene.score);
+          }
+          if (this.gameScene.enemies.children.length === 0) {
+            // this.parent.state = "winScreen";
+            // this.parent.showWinScreen(this.score, this.livesCount);
+          }
+        }
+      }
+
+      for (const enemy of this.gameScene.enemies.children) {
+        enemy.position.y += this.gameScene.enemySpeed * delay;
+        if (enemy.position.y >= this.gameWidth) {
+          enemy.position.y = 0 + delay;
+        }
+        for (
+          let index = 0;
+          index < this.gameScene.spaceShip.children.length;
+          index++
+        ) {
+          const player = this.gameScene.spaceShip.children[index];
+
+          if (enemy.getBounds().intersects(player.getBounds())) {
+            this.gameScene.enemies.removeChild(enemy);
+            this.gameScene.livesCount -= 1;
+            this.gameScene.score += 1;
+            this.gameScene.spaceShip.alpha = 0.5;
+            this.gameScene.isDamaged = true;
+            this.gameScene.lastCollisionTime = Date.now();
+            this.gameScene.stats.updateScore(this.gameScene.score);
+            this.gameScene.lives.loseLife();
+          }
+
+          if (this.gameScene.enemies.children.length === 0) {
+            this.state = "winScreen";
+            this.showWinScreen(this.gameScene.score, this.gameScene.livesCount);
+          }
+        }
+      }
+      if (this.gameScene.livesCount === 0) {
+        this.state = "gameOver";
+        this.showLoseScreen(this.gameScene.score);
+      }
+    };
+  }
+
   showWinScreen(score: number, livesCount: number) {
     const winField = new WinField(
       this.gameWidth,
@@ -91,9 +214,10 @@ export default class App extends Application {
     this.stage.addChild(restartField);
     restartField.on("click", () => {
       this.state = "mainMenu";
-      for (const child of this.gameScene.children.slice()) {
-        this.gameScene.removeChild(child);
+      for (const child of this.stage.children.slice()) {
+        this.stage.removeChild(child);
       }
+
       this.setup();
     });
   }
--- src/ts/GameScene.ts
+++ src/ts/GameScene.ts
@@ -28,6 +28,7 @@ export default class GameScene extends Container {
   livesCount: number;
   spaceShip: SpaceShip;
   bullets: Bullets;
+
   constructor(
     gameWidth: number,
     gameHeight: number,
@@ -53,7 +54,6 @@ export default class GameScene extends Container {
     this.isDamaged = false;
     this.score = 0;
     this.livesCount = livesCount;
-
     this.background = new Background("../resources/bcg.png");
     this.spaceShip = new SpaceShip(
       this.gameWidth,
@@ -74,119 +74,4 @@ export default class GameScene extends Container {
       "../resources/heart.png"
     );
   }
-
-  setup() {
-    document.onkeydown = (event) => {
-      this.keysMaps[event.code] = true;
-    };
-
-    document.onkeyup = (event) => {
-      this.keysMaps[event.code] = false;
-    };
-
-    document.onmousedown = () => {
-      this.isMouseFlag = true;
-    };
-
-    document.onmouseup = () => {
-      this.isMouseFlag = false;
-    };
-
-    this.addChild(this.background);
-    this.addChild(this.spaceShip);
-    this.addChild(this.bullets);
-    this.addChild(this.enemies);
-    this.addChild(this.stats);
-    this.addChild(this.lives);
-    return (delay: number) => {
-      if (this.keysMaps["ArrowLeft"]) {
-        this.spaceShip.moveSpriteLeft(delay);
-      }
-      if (this.keysMaps["ArrowRight"]) {
-        this.spaceShip.moveSpriteRight(delay);
-      }
-      if (this.keysMaps["ArrowUp"]) {
-        this.spaceShip.moveSpriteUp(delay);
-      }
-      if (this.keysMaps["ArrowDown"]) {
-        this.spaceShip.moveSpriteDown(delay);
-      }
-
-      if (this.isMouseFlag) {
-        const currentTime = Date.now();
-        if (currentTime - this.lastBulletSpawnTime > this.spawnSpeed) {
-          this.bullets.spawnBullet(this.spaceShip);
-          this.lastBulletSpawnTime = currentTime;
-        }
-      }
-
-      if (this.isDamaged) {
-        const currentTime = Date.now();
-        const checkIfTimePassed =
-          currentTime - this.lastCollisionTime > this.collisionEffectDuration;
-
-        if (checkIfTimePassed) {
-          this.spaceShip.alpha = 1;
-          this.isDamaged = true;
-          this.lastCollisionTime = 0;
-        }
-      }
-
-      for (let index = 0; index < this.bullets.children.length; index += 1) {
-        const bullet = this.bullets.children[index];
-        bullet.position.y -= this.bulletSpeed * delay;
-
-        if (bullet.position.y < 0) {
-          this.bullets.removeChild(bullet);
-          continue;
-        }
-
-        for (const enemy of this.enemies.children) {
-          if (enemy.getBounds().intersects(bullet.getBounds())) {
-            this.enemies.removeChild(enemy);
-            this.score += 1;
-            this.stats.updateScore(this.score);
-          }
-          if (this.enemies.children.length === 0) {
-            // this.state = "winScreen";
-            // this.showWinScreen(this.score, this.livesCount);
-          }
-        }
-      }
-
-      for (const enemy of this.enemies.children) {
-        enemy.position.y += this.enemySpeed * delay;
-        if (enemy.position.y >= this.gameWidth) {
-          enemy.position.y = 0 + delay;
-        }
-        for (let index = 0; index < this.spaceShip.children.length; index++) {
-          const player = this.spaceShip.children[index];
-
-          if (enemy.getBounds().intersects(player.getBounds())) {
-            this.enemies.removeChild(enemy);
-            this.livesCount -= 1;
-            this.score += 1;
-            this.spaceShip.alpha = 0.5;
-            this.isDamaged = true;
-            this.lastCollisionTime = Date.now();
-            this.stats.updateScore(this.score);
-            this.lives.loseLife();
-          }
-
-          if (this.enemies.children.length === 0) {
-            // this.state = "winScreen";
-            // this.showWinScreen(score, livesCount);
-          }
-        }
-      }
-      if (this.livesCount === 0) {
-        // this.state = "gameOver";
-        // this.showLoseScreen(this.score);
-      }
-    };
-  }
-
-  // showWinScreen(score: number, livesCount: number) {
-
-  // }
 }
