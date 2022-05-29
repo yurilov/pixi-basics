7c70724e23c9e661bb598f187dc24e329046c0d6
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-05-29 21:52:48 +0300

    added scene mamager and changes to loader scene

--- src/ts/Loader/LoaderScene.ts
+++ src/ts/Loader/LoaderScene.ts
@@ -1,20 +1,24 @@
 import { Container, Graphics, Loader } from "pixi.js";
-import { assets } from "../assets";
+import { IScene, SceneManager } from "../Manager/Manager";
 
-export class LoaderScene extends Container {
+import { assets } from "../assets";
+import { GameScene } from "../GameScene";
+
+export class LoaderScene extends Container implements IScene {
 
     private loaderBar: Container;
     private loaderBarBorder: Graphics;
     private loaderBarFill: Graphics;
-    constructor(screenWidth: number, screenHeight: number) {
+    constructor() {
         super();
 
-        const loaderBarWidth = screenWidth * 0.8;
+        const loaderBarWidth = SceneManager.width * 0.8;
+
         this.loaderBarFill = new Graphics();
         this.loaderBarFill.beginFill(0x008800, 1)
         this.loaderBarFill.drawRect(0, 0, loaderBarWidth, 50);
         this.loaderBarFill.endFill();
-        this.loaderBarFill.scale.x = 0; // we draw the filled bar and with scale we set the %
+        this.loaderBarFill.scale.x = 0;
 
         this.loaderBarBorder = new Graphics();
         this.loaderBarBorder.lineStyle(10, 0x0, 1);
@@ -23,17 +27,15 @@ export class LoaderScene extends Container {
         this.loaderBar = new Container();
         this.loaderBar.addChild(this.loaderBarFill);
         this.loaderBar.addChild(this.loaderBarBorder);
-        this.loaderBar.position.x = (screenWidth - this.loaderBar.width) / 2; 
-        this.loaderBar.position.y = (screenHeight - this.loaderBar.height) / 2;
+        this.loaderBar.position.x = (SceneManager.width - this.loaderBar.width) / 2;
+        this.loaderBar.position.y = (SceneManager.height - this.loaderBar.height) / 2;
         this.addChild(this.loaderBar);
-
 
         Loader.shared.add(assets);
 
         Loader.shared.onProgress.add(this.downloadProgress, this);
         Loader.shared.onComplete.once(this.gameLoaded, this);
 
-     
         Loader.shared.load();
     }
 
@@ -43,6 +45,10 @@ export class LoaderScene extends Container {
     }
 
     private gameLoaded(): void {
-        this.removeChild(this.loaderBar);
+        SceneManager.changeScene(new GameScene());
+    }
+
+    public update(framesPassed: number): void {
+        // must have
     }
 }
