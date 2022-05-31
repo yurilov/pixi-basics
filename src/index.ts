fa4d6df486c18eedc62da21fc381a40feac42c8f
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-05-31 17:52:44 +0300

    deleted unused files and folders

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,25 @@
 import SceneManager from "./scenes/sceneManager/SceneManager";
 import LoaderScene from "./scenes/loaderScene/LoaderScene";
-import Stats from "stats.js";
+// import Stats from "stats.js";
 
-// const canvas = document.querySelector("canvas");
 const gameWidth = 1000;
 const gameHeight = 600;
-
-// const settings = {
-//   view: canvas,
-//   width: gameWidth,
-//   height: gameHeight,
-//   backgroundColor: 138298,
-// };
-
-// const app = new App(settings);
-// app.setup();
 
 SceneManager.initialize(gameWidth, gameHeight, 138298);
 
 const loaderScene: LoaderScene = new LoaderScene();
 SceneManager.changeScene(loaderScene);
 
-const stats = new Stats();
-stats.showPanel(0);
-document.body.appendChild(stats.dom);
+// const stats = new Stats();
+// stats.showPanel(0);
+// document.body.appendChild(stats.dom);
 
-function animate() {
-  stats.begin();
+// function animate() {
+//   stats.begin();
 
-  stats.end();
+//   stats.end();
 
-  requestAnimationFrame(animate);
-}
+//   requestAnimationFrame(animate);
+// }
 
-requestAnimationFrame(animate);
+// requestAnimationFrame(animate);
